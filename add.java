a6d057c0dae49cd45f0872384b1f308bd2532f65
anne-debug <annelovedance0513@gmail.com>
2020-03-10 15:03:07 +0800

    add number

---
+++ add.java
@@ -0,0 +1,8 @@
+public class add {
+    public static void main(String[] args) {
+        int n1 = 1;
+        int n2 = 2;
+        int total = n1 + n2;
+        System.out.println(total);
+    }
+}

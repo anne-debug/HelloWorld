0570158b0f4f2ed43198d527bfdd34cc6e6af29a
anne-debug <annelovedance0513@gmail.com>
2020-03-29 20:32:49 +0800

    add value of a, b

--- add.java
+++ add.java
@@ -3,6 +3,8 @@ public class add {
         int n1 = 1;
         int n2 = 2;
         int total = n1 + n2;
+  	System.out.println(n1);
+	System.out.println(n2);
         System.out.println(total);
     }
 }
